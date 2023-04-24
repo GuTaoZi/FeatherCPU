adf6b2d992200514a95aa59c5dbfce440f48b92c
jayfeather233 <jayfeather233@gmail.com>
2023-04-24 15:09:27 +0800

    ALU v0.0.2

--- src/FeatherCPU.srcs/sources_1/new/ALU.v
+++ src/FeatherCPU.srcs/sources_1/new/ALU.v
@@ -26,7 +26,8 @@ input [`REG_WIDTH]    src1,
 input [`REG_WIDTH]    src2,
 input [`ALU_OP_LEN]   ALU_op,
 input                 rst,
-output [`REG_WIDTH]   opt
+output [`REG_WIDTH]   opt,
+output                overflow
     );
 wire [`REG_WIDTH] sum;
 wire [`REG_WIDTH] mul;
@@ -36,9 +37,9 @@ wire [`REG_WIDTH] slt;
 
 assign sum      = ALU_op[0] ? src1 - src2 : src1 + src2;
 assign mul      = ALU_op[0] ? src1 / src2 : (ALU_op[1] ? src1 % src2 : src1 * src2);
-assign shift    = ALU_op[1] ? (ALU_op[0] ? {src1[31], src1[30:0]} : {1'b0, src1[30:0]}) : {src1[31:1], 1'b0};
+assign shift    = ALU_op[1] ? (ALU_op[0] ? (src1>>>src2) : (src1>>src2)) : (src1<<src2);
 assign binary   = ALU_op[0] ? (src1 | src2) : (ALU_op[1] ? (src1 ^ src2) : (src1 & src2));
-assign slt      = src1 < src2 ? 1 : 0;
+assign slt      = ALU_op[0] ? ($signed(src1) < $signed(src2) ? 1 : 0) : (src1 < src2 ? 1 : 0);
 
 assign opt      = rst ? 0 : (
     (ALU_op ^ `ALU_ADD) >> 2 == 0 ? sum :
@@ -46,6 +47,12 @@ assign opt      = rst ? 0 : (
     (ALU_op ^ `ALU_SLL) >> 2 == 0 ? shift :
     (ALU_op ^ `ALU_AND) >> 2 == 0 ? binary :
     (ALU_op ^ `ALU_SLT) >> 2 == 0 ? slt :
+    0
+);
+
+assign overflow = rst ? 0 : (
+    (ALU_op == `ALU_ADD) ? ((src1[`REG_MAX_LEN]&src2[`REG_MAX_LEN]&~sum[`REG_MAX_LEN]) | (~src1[`REG_MAX_LEN]&~src2[`REG_MAX_LEN]&sum[`REG_MAX_LEN])) :
+    (ALU_op == `ALU_SUB) ? ((src1[`REG_MAX_LEN]&~src2[`REG_MAX_LEN]&~sum[`REG_MAX_LEN]) | (~src1[`REG_MAX_LEN]&src2[`REG_MAX_LEN]&sum[`REG_MAX_LEN])) :
     0
 );
 endmodule
