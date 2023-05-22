416f075273bad07a0ec599a1c953dee8f7e95215
jayfeather233 <jayfeather233@gmail.com>
2023-05-22 15:31:12 +0800

    uart update

---
+++ src/FeatherCPU.ip_user_files/sim_scripts/uart0_1/xcelium/run.f
@@ -0,0 +1,15 @@
+-makelib xcelium_lib/xpm -sv \
+  "C:/Xilinx/Vivado/2022.2/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib xcelium_lib/xpm \
+  "C:/Xilinx/Vivado/2022.2/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../FeatherCPU.gen/sources_1/ip/uart0_1/uart_bmpg.v" \
+  "../../../../FeatherCPU.gen/sources_1/ip/uart0_1/upg.v" \
+  "../../../../FeatherCPU.gen/sources_1/ip/uart0_1/sim/uart0.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
--- src/FeatherCPU.runs/uart0_synth_1/uart0.tcl
+++ src/FeatherCPU.runs/uart0_synth_1/uart0.tcl
@@ -85,13 +85,13 @@ set_property parent.project_path C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCP
 set_property XPM_LIBRARIES XPM_MEMORY [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_repo_paths c:/Users/jayfe/Desktop/course/DTwo/CO/SEU_CSE_507_user_uart_bmpg_1.3 [current_project]
+set_property ip_repo_paths c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/SEU_CSE_507_user_uart_bmpg_1.3 [current_project]
 update_ip_catalog
 set_property ip_output_repo c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
-read_ip -quiet c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.srcs/sources_1/ip/uart0/uart0.xci
+read_ip -quiet c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.srcs/sources_1/ip/uart0_1/uart0.xci
 
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
@@ -160,32 +160,32 @@ create_report "uart0_synth_1_synth_report_utilization_0" "report_utilization -fi
 OPTRACE "synth reports" END { }
 
 if { [catch {
-  file copy -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0.dcp c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0.dcp
+  file copy -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0.dcp c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 status "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  write_verilog -force -mode synth_stub c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0_stub.v
+  write_verilog -force -mode synth_stub c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode synth_stub c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0_stub.vhdl
+  write_vhdl -force -mode synth_stub c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_verilog -force -mode funcsim c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0_sim_netlist.v
+  write_verilog -force -mode funcsim c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode funcsim c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0_sim_netlist.vhdl
+  write_vhdl -force -mode funcsim c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -195,32 +195,32 @@ if { [catch {
 
 
 if { [catch {
-  file copy -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0.dcp c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0.dcp
+  file copy -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0.dcp c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 status "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  file rename -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0_stub.v c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0_stub.v
+  file rename -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0_stub.v c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0_stub.vhdl c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0_stub.vhdl
+  file rename -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0_stub.vhdl c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0_sim_netlist.v c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0_sim_netlist.v
+  file rename -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0_sim_netlist.v c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0_sim_netlist.vhdl c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0_sim_netlist.vhdl
+  file rename -force C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.runs/uart0_synth_1/uart0_sim_netlist.vhdl c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -229,13 +229,13 @@ if { [catch {
 
 if {[file isdir C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.ip_user_files/ip/uart0]} {
   catch { 
-    file copy -force c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0_stub.v C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.ip_user_files/ip/uart0
+    file copy -force c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0_stub.v C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.ip_user_files/ip/uart0
   }
 }
 
 if {[file isdir C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.ip_user_files/ip/uart0]} {
   catch { 
-    file copy -force c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0/uart0_stub.vhdl C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.ip_user_files/ip/uart0
+    file copy -force c:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.gen/sources_1/ip/uart0_1/uart0_stub.vhdl C:/Users/jayfe/Desktop/course/DTwo/CO/FeatherCPU/src/FeatherCPU.ip_user_files/ip/uart0
   }
 }
 file delete __synthesis_is_running__
